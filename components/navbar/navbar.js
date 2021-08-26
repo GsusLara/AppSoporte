6a108832f0a8a984323e5b1d184005666f46505a
GsusLara <jelara99@gmail.com>
2021-08-26 07:15:13 -0600

    diseño completo

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                    <img src="/Fod.png" alt="Fundacion Omar Dengo" className="logo p-1 mr-1 navbar-brand" />
+                <img src="/Fod.png" alt="Fundacion Omar Dengo" className="logo p-1 mr-1 navbar-brand" />
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -20,7 +20,7 @@ export default function Navbar() {
                         </li>
                         <li className="nav-item ">
                             <Link href="/modCarga">
-                                <a className="nav-link" href="#">Cargas de Trabajo</a>
+                                <a className="nav-link" >Cargas de Trabajo</a>
                             </Link>
                         </li>
                         <li className="nav-item">
@@ -30,10 +30,10 @@ export default function Navbar() {
                             <a className="nav-link " href="#" >Usuarios</a>
                         </li>
                     </ul>
-                    <div className="collapse navbar-collapse"></div>
-                    <span style={{ color: "#00958F" }}>
-                        <strong>Soporte-Cedi</strong>
-                    </span>
+                    <div className="collapse navbar-collapse" />
+                    <Link href="/">
+                        <a className="nav-link salir" >Cerrar Sesión</a>
+                    </Link>
                 </div>
             </div>
         </nav>
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import Particles from 'react-particles-js';
 
 export default function Login() {
   return (
-    <div className="container">
-      <div className="row justify-content-md-center boxlogin">
-        <div className="col-4 arealogin">
-          <h2 className="text-center mb-3">Bienvenido</h2>
+    <div className="container-fluid caja">
+      <Decoracion />
+      <div className="row justify-content-center cajalogin">
+        <div className="col-8 col-lg-4 col-xl-3 arealogin">
+          <h2 className="text-center mt-3 mb-3">Soporte CEDI</h2>
           <form>
             <div className="mb-3">
               <label htmlFor="exampleInputEmail1" className="form-label ">Correo Electronico</label>
@@ -17,11 +19,68 @@ export default function Login() {
               <input type="password" className="form-control" id="exampleInputPassword1" />
             </div>
             <Link href="/dashboard">
-              <a type="submit" className="btn btn-success mt-2">Ingresar</a>
+              <a type="submit" className="btn btn-success mt-2 mb-3">Ingresar</a>
             </Link>
           </form>
         </div>
       </div>
     </div>
   )
+}
+function Decoracion() {
+  return (
+    <Particles
+      params={{
+        "particles": {
+          "number": {
+            "value": 160,
+            "density": {
+              "enable": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "speed": 4,
+              "size_min": 0.3
+            }
+          },
+          "line_linked": {
+            "enable": false
+          },
+          "move": {
+            "random": true,
+            "speed": 1,
+            "direction": "top",
+            "out_mode": "out"
+          }
+        },
+        "interactivity": {
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "repulse"
+            }
+          },
+          "modes": {
+            "bubble": {
+              "distance": 250,
+              "duration": 2,
+              "size": 0,
+              "opacity": 0
+            },
+            "repulse": {
+              "distance": 400,
+              "duration": 4
+            }
+          }
+        }
+      }} />
+  )
+
 }
