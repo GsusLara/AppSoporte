b5b2836763519d84f937603e60bb4b76dcbe4200
GsusLara <jelara99@gmail.com>
2021-09-02 15:38:59 -0600

    informes

---
+++ pages/modInformes/modInformes.js
@@ -0,0 +1,9 @@
+export default function modInformes() {
+    return (
+        <div className="row">
+            <div className="col-12">
+                <h1>Generacion de Informes</h1>
+            </div>
+        </div>
+    )
+}
