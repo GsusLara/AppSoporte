cceba93d52675a1ab2fdf57a027fc550e2c31eee
Jesus Lara <jelara99@gmail.com>
2021-09-02 23:19:22 -0600

    correcciones para build numero cuatro

--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,10 @@ class MyDocument extends Document {
                     <meta charSet="utf-8" />
                     <meta name="autor " content="Jesus Lara" />
                     <link rel="icon" href="/favicon.ico" />
-                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"></link>
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
                 </body>
             </Html>
         )
