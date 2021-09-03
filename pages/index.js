3326813dd916df1e73d509c54bf014405e98d181
Jesus Lara <jelara99@gmail.com>
2021-09-02 23:09:51 -0600

    correcciones para build numero dos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Login from "./login";
+import Script from 'next/script'
 
 export default function Home() {
-  return  <Login/>
+  return (
+    <Login>
+    <Script src="https://third-party-script.js"></Script>
+    </Login>
+  ) 
 }
